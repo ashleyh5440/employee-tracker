6be3905c4c595f7be99102d7732c43075a719993
Ashley <ashleyh5440@gmail.com>
2023-12-13 15:41:54 -0500

    added inquierer questions and if/else statements in index.js

--- index.js
+++ index.js
@@ -1,17 +1,85 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+const inquirer = require("inquirer");
+const express = require("express");
+
+//questions for user
+inquirer
+  .prompt([
+      {
+        type: 'list',
+        name: 'prompt',
+        message: "What would you like to do?",
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+      },
+  ])
+
+//need to have multiple if/else statements
+//if "view departments", show table with department names and ids
+//if "view all roles", show job title, role id, the department, and salary 
+//if "view all employees", show table with employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+//if "add a department", user enters new department name
+//if "add a role", user enters name, salary, and department for new role
+//if "add employee", user enters employee’s first name, last name, role, and manager
+//if "update", user selects which employee
+  .then((answers) => {
+    if(answers.prompt === "View all departments"){
+
+    }else if(answers.prompt === "View all roles"){
+
+    }else if(answers.prompt === "View all employees"){
+
+    }else if(answers.promt === "Add a department"){
+        inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'new department',
+            message: "What is the department name?",
+        },
+  ])
+    }else if(answers.prompt === "Add a role"){
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'role name',
+                message: "What is the name of this new role?",
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: "What is the salary for this new role?",
+            },
+            {
+                type: 'input',
+                name: 'department',
+                message: "What department will this new role be under?",
+            }
+      ])
+    }else if(answers.prompt === "Add an employee"){
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'first name',
+                message: "Please enter the employee's first name.",
+            },
+            {
+                type: 'input',
+                name: 'last name',
+                message: "Please enter the employee's last name.",
+            },
+            {
+                type: 'input',
+                name: 'role',
+                message: "Please enter the employee's role.",
+            },
+            {
+                type: 'input',
+                name: 'manager',
+                message: "Please enter the employee's manager.",
+            }
+      ])
+    }else(answers.prompt === "Update an employee role"){
+
+    }
+   })
